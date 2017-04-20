f8931a7091c71ad8a8a3d572cf79bafbe43adb1f
sunerok <justinvforvendetta@gmail.com>
2017-04-20 14:08:55 -0400

    initial commit

---
+++ qt/radio.cpp
@@ -0,0 +1,31 @@
+#include "ui_radio.h"
+//the folling includes apply only to blockchain qt applications
+//#include "guiutil.h"
+//#include "bitcoingui.h"
+//#include "util.h"
+//#include "main.h"
+#include <QtCore>
+#include <QtGui>
+#include <QtWebKit>
+#include <QtWidgets>
+#include <QtWebKit/QtWebKit>
+#include <QtWebKitWidgets/QtWebKitWidgets>
+
+Radio::Radio(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Radio),
+    model(0)
+{
+    ui->setupUi(this);
+}
+    void Radio::setModel(WalletModel *model)
+{
+    this->model = model;
+    if(!model)
+        return;
+}
+
+Radio::~Radio()
+{
+    delete ui;
+} 
